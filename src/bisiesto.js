b52ae00801936ed77bf5b5bd2690fa2b32b56f66
Walter Rocha <walterrochasevilla@gmail.com>
2024-08-25 19:23:01 -0400

    Divisible a 4 pero no a 100

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,7 +1,8 @@
 function es_bisiesto(n) {
     let resp = false;
     if (n % 400 == 0)  resp = true ;
-    else if (n % 100 && n % 400 == 0)  resp = false ;
+    else if (n % 100 == 0)  resp = false ;
+    else if (n % 4 == 0)  resp = true ;
     return resp;
   }
   
--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -5,6 +5,9 @@ describe("Bisiesto", () => {
     expect(es_bisiesto(2000)).toEqual(true);
   });
   it("Debería devolver false si es dividendo de 100 pero no de 400", () => {
-    expect(es_bisiesto(2100 )).toEqual(false);
+    expect(es_bisiesto(2100)).toEqual(false);
+  });
+  it("Debería devolver true si es dividendo de 4 pero no de 100", () => {
+    expect(es_bisiesto(2016)).toEqual(true);
   });
 });
